1048ebd7e8fc02b7bed58ad5271192097e028c8d
Daria Yats <dariayatsyniuk@gmail.com>
2024-10-15 16:42:00 +0200

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Дарья Яцынюк on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
