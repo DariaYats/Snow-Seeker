58302af5d7db3730aaa77ebc189a09f03b8d7a5f
Daria Yats <dariayatsyniuk@gmail.com>
2024-10-30 13:35:32 +0100

    create a primary list of items

---
+++ SnowSeeker/WelcomeView.swift
@@ -0,0 +1,24 @@
+//
+//  WelcomeView.swift
+//  SnowSeeker
+//
+//  Created by Дарья Яцынюк on 28.10.2024.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to SnowSeeker!")
+                .font(.largeTitle)
+            
+            Text("Please select a resort from the left-hand menu; swipe from the left edge to show it.")
+                .foregroundStyle(.secondary)
+        }
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
